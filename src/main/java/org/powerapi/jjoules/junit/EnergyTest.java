8c32049d81c330626f68e3eb2a2f3de0b311d42e
Mamadou <msanoussy98@gmail.com>
2020-08-05 13:08:17 +0200

    add after all call back to generate energy reports for each test class

--- src/main/java/org/powerapi/jjoules/junit/EnergyTest.java
+++ src/main/java/org/powerapi/jjoules/junit/EnergyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,7 +15,7 @@ import java.lang.annotation.Target;
  * Annotation to trigger a test with energy sampling enabled (if available)
  *
  */
-@Target({METHOD, ANNOTATION_TYPE})
+@Target({TYPE, METHOD, ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(EnergyTestExtension.class)
 @Test
--- src/main/java/org/powerapi/jjoules/junit/EnergyTestExtension.java
+++ src/main/java/org/powerapi/jjoules/junit/EnergyTestExtension.java
@@ -3,9 +3,11 @@
  */
 package org.powerapi.jjoules.junit;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -18,8 +20,11 @@ import org.powerapi.jjoules.rapl.RaplDevice;
  * JUnit extension for logging the energy consumption of tests.
  *
  */
-public class EnergyTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback{
+public class EnergyTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback, AfterAllCallback{
 	private final Namespace NAMESPACE = Namespace.create(getClass());
+
+	public static final ReportRegister REPORT_REGISTER= new ReportRegister();
+	public static Map<String,Map<String, Long>> energyReports = new HashMap<String,Map<String, Long>>();
 
 	private final Store getStore(final ExtensionContext context) {
 		return context.getStore(this.NAMESPACE);
@@ -27,14 +32,26 @@ public class EnergyTestExtension implements BeforeTestExecutionCallback, AfterTe
 
 	@Override
 	public void beforeTestExecution(final ExtensionContext context) throws Exception {
-		getStore(context).put(context.getRequiredTestMethod(), RaplDevice.RAPL.recordEnergy());
+		
+		if(context.getRequiredTestMethod().isAnnotationPresent(EnergyTest.class))
+			getStore(context).put(context.getRequiredTestMethod(), RaplDevice.RAPL.recordEnergy());
 	}
 
 	@Override
 	public void afterTestExecution(final ExtensionContext context) throws Exception {
-		Map<String, Long> report = getStore(context).get(context.getRequiredTestMethod(), EnergySample.class).stop();
-		for (Entry<String, Long> value : report.entrySet()) {
-			context.publishReportEntry(value.getKey(), value.getValue().toString());
+		if(context.getRequiredTestMethod().isAnnotationPresent(EnergyTest.class)) {
+			Map<String, Long> report = getStore(context).get(context.getRequiredTestMethod(), EnergySample.class).stop();
+			energyReports.put(context.getRequiredTestMethod().getName(), report);
 		}
+		//		for (Entry<String, Long> value : report.entrySet()) {
+		//			context.publishReportEntry(value.getKey(), value.getValue().toString());
+		//		}
+	}
+
+	@Override
+	public void afterAll(ExtensionContext context) throws Exception {
+
+		REPORT_REGISTER.setFilename(context.getRequiredTestClass().getName());
+		REPORT_REGISTER.jsonRegistreReport(energyReports);
 	}
 }
