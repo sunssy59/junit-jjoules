90ee4ded548308035c124f4cc0ded6fef7e2a69f
Mamadou59 <msanoussy98@gmail.com>
2020-08-31 10:07:11 +0200

    add callgraph json file register

--- src/main/java/org/powerapi/jjoules/callgraph/CallGraphMojo.java
+++ src/main/java/org/powerapi/jjoules/callgraph/CallGraphMojo.java
@@ -24,6 +24,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import com.google.gson.Gson;
+
 /**
  * 
  *
@@ -34,12 +36,15 @@ public class CallGraphMojo extends AbstractMojo{
 
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
 	MavenProject project;
-	
+
 	@Parameter(defaultValue = "${project.build.directory}/call-graph", required = true)
 	private String outputDir;
 
 	@Parameter(defaultValue = "callgraph-matrix.csv" , required = true)
 	private String outputFilename;
+	
+	@Parameter(defaultValue = "callgraph-matrix.json" , required = true)
+	private String outputJSONFilename;
 
 	@Parameter(defaultValue = "tests-callgraph.txt", required = true)
 	private String callgraphFilename;
@@ -48,27 +53,34 @@ public class CallGraphMojo extends AbstractMojo{
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		
+
 		File outputDirFile = new File(outputDir);
 		if (! outputDirFile.exists())
 			outputDirFile.mkdir();
 
 		File outputFile = new File(outputDir,outputFilename);
 		File callgraphFile = new File(outputDir,callgraphFilename);
-
-		this.executes(callgraphFile,outputFile);
+		
+		File callgraphJSONFile = new File(outputDir,outputJSONFilename);
+		
+		this.executes(callgraphFile,outputFile,callgraphJSONFile);
 	}
 
-	private void executes(File callgraphFile, File outputFile) {
+	private void executes(File callgraphFile, File outputFile,File callgraphJSONFile) {
 
 		getLog().info("Parsing tests call graph in file {"+callgraphFilename+"}...");
 		this.readCallgraphFile(callgraphFile);
 		getLog().info("End parsing file {"+callgraphFilename+"}");
-		
-		
+
+
 		getLog().info("Creation of the call graph matrix...");
 		this.createCallgraphMatrix(outputFile);
 		getLog().info("Call graph matrix created and dumped on file {"+outputFilename+"}");
+		
+		getLog().info("Writing callgraph for energy test in json file...");
+		callgraphRegisterInJSON(callgraphJSONFile);
+		getLog().info("Callgraph data for energy test are dumped on json file {"+outputJSONFilename+"}");
+	
 	}
 
 	private void readCallgraphFile(File callgraphFile) {
@@ -89,6 +101,7 @@ public class CallGraphMojo extends AbstractMojo{
 				String tgt = lineSplited[1];
 
 				if (src.startsWith("M") && 
+						(src.contains("EnergyTest")) &&
 						(tgt.contains(project.getGroupId())) &&
 						(tgt.startsWith(("(M")) || 
 								tgt.startsWith(("(S")) )) 
@@ -105,6 +118,29 @@ public class CallGraphMojo extends AbstractMojo{
 			getLog().error("You may have to move your file {"+callgraphFilename+"} in directory {"+outputDir+"}");
 		}
 	}
+
+
+	private void callgraphRegisterInJSON(File callgraphJSONFile) {
+
+		Gson gson = new Gson();
+
+		if(! callgraphJSONFile.exists()) {
+			try {
+				callgraphJSONFile.createNewFile();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileWriter fw = new FileWriter(callgraphJSONFile.getAbsoluteFile());
+			gson.toJson(gson.toJsonTree(this.methodsCallgraph),fw);
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	private void createCallgraphMatrix(File outputFile) {
 
@@ -128,16 +164,16 @@ public class CallGraphMojo extends AbstractMojo{
 			bw.write("methods,");
 			int cpt = 0;
 			for(String key : keys){
-				
+
 				if (cpt == keys.size()-1)
 					bw.write(key);
 				else 
 					bw.write(key+",");
 				cpt++;
 			}
-			
+
 			bw.newLine();
-			
+
 			// writing all values lines
 			String valueToWrite ="";
 			for(String value : values) {
@@ -145,14 +181,14 @@ public class CallGraphMojo extends AbstractMojo{
 				cpt =0;
 				for(String key : keys) {
 					valueToWrite = this.methodsCallgraph.get(key).contains(value) ? "true" : "false"; 
-					
+
 					valueToWrite = (cpt == keys.size()-1) ? valueToWrite : valueToWrite + ",";
-					
+
 					bw.write(valueToWrite);
-					
+
 					cpt++;
-					
-					
+
+
 				}
 				bw.newLine();
 			}
