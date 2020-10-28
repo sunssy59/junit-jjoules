d0da5f4dbed0ecbc1949d82da50bb7ea16f31d07
Benjamin Danglot <bdanglot@gmail.com>
2020-10-28 15:43:20 +0100

    test: add some weak tests

---
+++ src/test/java/org/powerapi/jjoules/AbstractTest.java
@@ -0,0 +1,31 @@
+package org.powerapi.jjoules;
+
+import org.junit.After;
+import org.junit.jupiter.api.AfterAll;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * 28/10/2020
+ */
+public abstract class AbstractTest {
+
+    private static final String FOLDER_PATH = "target/jjoules-reports/";
+
+    protected List<String> pathToAssert = new ArrayList<>();
+
+    @After
+    public void tearsDown() {
+        for (String path : this.pathToAssert) {
+            final File reportFile = new File(FOLDER_PATH, path);
+            assertTrue(reportFile.exists());
+        }
+    }
+
+}
---
+++ src/test/java/org/powerapi/jjoules/junit/Test.java
@@ -0,0 +1,24 @@
+package org.powerapi.jjoules.junit;
+
+import org.powerapi.jjoules.AbstractTest;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * 28/10/2020
+ */
+public class Test extends AbstractTest {
+
+    @EnergyTest
+    public void method() {
+        System.out.println("1");
+        this.pathToAssert.add("org.powerapi.jjoules.junit.Test-method1.json");
+    }
+
+    @EnergyTest
+    public void method2() {
+        System.out.println("2");
+        this.pathToAssert.add("org.powerapi.jjoules.junit.Test-method2.json");
+    }
+
+}
